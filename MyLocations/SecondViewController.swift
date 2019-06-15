09e53e8ef7cc85c634e20a6aa38ae81b867285a1
Bane Manojlovic <bane1manojlovic@gmail.com>
2019-06-15 19:38:58 +0200

    Initial Commit

---
+++ MyLocations/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  MyLocations
+//
+//  Created by Bane Manojlovic on 6/15/19.
+//  Copyright © 2019 Bane Manojlovic. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
